b63d81d3f94b8913e1f6b9077fcc10c899809170
Raviprasaath Ramesh <raviprasaathramesh@gmail.com>
2026-04-06 16:30:47 +0530

    update

---
+++ Season1/01-basics.js
@@ -0,0 +1,9 @@
+/*
+ 
+why node js
+In the beginning apache server was used to run js on server but it was not efficient and fast as it was single threaded and blocking. 
+So Ryan Dahl created node js which is built on chrome v8 engine and it is non blocking and event driven.
+
+Node JS is 
+
+*/
